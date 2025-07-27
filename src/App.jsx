36a5fccd3d044084d6e9dd2ffa48e438054db3be
Dhruv Gadre <dhruvgadre09062005@gmail.com>
2025-07-28 01:04:32 +0530

    Added Basic Header Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/signIn";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
       </Routes>
