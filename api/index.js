e9efa76f86c84e6cb4ef5f5795959e588d84ddc9
Dhruv Gadre <dhruvgadre09062005@gmail.com>
2025-08-25 21:54:05 +0530

    created middleware and function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -24,5 +24,15 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
 })
 
-app.use("/api/user", userRouter); 
+app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;  //Internal server error
+    const message = err.message || "Internal Server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
